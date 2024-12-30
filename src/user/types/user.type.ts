cea490d8dc0737bfc11b7eeee1de0c0053e2d719
Israel Adekanmi <adekanmiisrael8@gmail.com>
2024-12-30 09:49:31 +0100

    feat: Integrating User & Product Backend Service

---
+++ src/user/types/user.type.ts
@@ -0,0 +1,17 @@
+export enum Gender {
+    Male = 'Male',
+    Female = 'Female',
+    NotSpecified = 'Not Specified',
+  }
+  
+  export enum SignUpType {
+    admin = 'admin',
+    customer = 'customer',
+    vendor = 'vendor',
+  }
+  
+  export enum Role {
+    Customer = 'customer',
+    Vendor = 'vendor',
+  }
+  
